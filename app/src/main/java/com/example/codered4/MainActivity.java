0b58448a2b8cffd80d3052361ec18f428eb99cb4
sudharsan-19 <sudharsanguna2001@gmail.com>
2022-04-01 11:00:43 +0530

    Login Design

--- app/src/main/java/com/example/codered4/MainActivity.java
+++ app/src/main/java/com/example/codered4/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.codered4;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView newReg;
+    Button subBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       //ABCD
+        newReg=findViewById(R.id.newReg);
+        subBtn=findViewById(R.id.loginbtn);
+        newReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent();
+                startActivity(i);
+            }
+        });
+        subBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i2 = new Intent();
+                startActivity(i2);
+            }
+        });
     }
 }
